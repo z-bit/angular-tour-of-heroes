7eb0cc61b1b68c6eca04629eeffcfcace6b1a891
admin <admin@Mac.local>
2016-09-23 22:49:53 +0100

    services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeroDetailComponent } from './hero-detail.component';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 
 
@@ -73,12 +75,29 @@ import { Hero } from './hero';
 
         <hero-detail [hero]="selectedHero"></hero-detail>
 
-    </div>`
+    </div>`,
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     title: string = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(
+        private heroService: HeroService
+    ) {}
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    getHeroes(): void {
+        this.heroService
+            //.getHeroes()
+            .getHeroesSlowly()
+            .then(heroes => this.heroes = heroes)
+        ;
+    }
 
     onSelect(hero) {
         this.selectedHero = hero;
@@ -88,18 +107,4 @@ export class AppComponent {
 
 }
 
-
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
 
---
+++ app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import {resolve} from "url";
+
+
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(
+            resolve => setTimeout(resolve, 2000)
+        ).then(
+            () => this.getHeroes()
+        );
+    }
+}
+
